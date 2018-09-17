60b3c18d446cd32c4362b6fdd510fd9fbad69f82
yue shuai <wsyueshuai@163.com>
2018-09-17 08:23:25 +0800

    seq_std

--- seq_std04.cpp
+++ seq_std04.cpp
@@ -37,6 +37,7 @@ int main(){
   //test2 resize
   //如果当前容器的大小大于所要求的大小，容器后部的元素会被删除；
   //如果当前容器的大小小于所要去的大小，会讲新元素添加到容器的后部
+  /*
   list<int> li(5,11);
   cout << li.size() << endl;
   for(auto s : li){
@@ -52,6 +53,29 @@ int main(){
   li.resize(3,8);//因为3小于原来容器的大小7，所以第二个参数8就被忽略了
   cout << li.size() << endl;
   for(auto s : li){
+    cout << s << " ";
+  }
+  cout << endl;
+  */
+
+  //test3 容器的插入删除操作可能使迭代器失效
+  vector<int> vi = {0,1,2,3,4,5,6,7,8,9};
+  auto it = vi.begin();
+  //这里每次都会重新去取得尾迭代器，不会有问题，
+  //如果提前把end保存了的话，改变vi后，end就失效了，会死循环等
+  while(it != vi.end()){
+    //奇数的时候
+    if(*it % 2){
+      it = vi.insert(it, *it);
+      //重新设定迭代器，让it指向下一个奇数
+      it += 2;
+    }
+    //偶数的时候
+    else{
+      it = vi.erase(it);
+    }
+  }
+  for(auto s : vi){
     cout << s << " ";
   }
   cout << endl;
